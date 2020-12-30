6db846495997227ae307115033cd3dbba254a97b
Michelle Comitor <74374088+MComitor@users.noreply.github.com>
2020-12-30 11:28:57 -0600

    Pushing my first coding attempt through for PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,8 +1,87 @@
-# In this challenge, you are tasked with creating a Python script for analyzing the financial records of your company. You will give a set of financial data called [budget_data.csv](PyBank/Resources/budget_data.csv). The dataset is composed of two columns: `Date` and `Profit/Losses`. (Thankfully, your company has rather lax standards for accounting so the records are simple.)
+# In this challenge, you are tasked with creating a Python script for analyzing the financial records of your company. 
+# You will give a set of financial data called [budget_data.csv](PyBank/Resources/budget_data.csv). 
+# The dataset is composed of two columns: `Date` and `Profit/Losses`. 
+# In addition, your final script should both print the analysis to the terminal and export a text file with the results.
+# As an example, your analysis should look similar to the one below:
 
-# Your task is to create a Python script that analyzes the records to calculate each of the following:
+#   Financial Analysis
+#   --------------------------------------------------
+#   Total Months: 86
+#   Total: $38382578
+#   Average Change: $-2315.12
+#   Greatest Increase in Profits: Feb-2012 ($1926159)
+#   Greatest Decrease in Profits: Sep-2013 ($-2196167)
+#   --------------------------------------------------
 
-# The total number of months included in the dataset
+
+# Import the necessary modules
+import os
+import csv
+import statistics
+
+
+# Path to my csv file
+csvpath = os.path.join('..', 'PyBank', 'Resources', 'budget_data.csv')
+
+with open(csvpath) as csvfile:
+
+# CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    # Read the header row first (skip this step if there is no header)
+    header = next(csvreader)
+
+# Create the lists to search through
+    profit_list = []
+    changes_list = []
+    periods_list = []
+    last_month = 0
+
+    # for row in csvreader:
+    for i, row in enumerate(csvreader):
+        profit_list.append(int(row[1]))
+        periods_list.append(row[0])
+        if i == 0:
+            changes_list.append(0)
+        else:
+            changes_list.append(int(row[1]) - last_month)
+    last_month = int(row[1])
+    total_profit = sum(profit_list)
+    max_profit = max(profit_list)
+    min_profit = min(profit_list)
+    avg_change = statistics.mean(changes_list)
+    total_months = len(periods_list)
+    print()
+    print(f"Financial Analysis")
+    print(f"--------------------------------------------------")
+    print(f"Total Months: {total_months}")
+    print(f"Total: ${total_profit}")
+    print(round(avg_change, 2))
+    # print(f"Greatest Increase in Profits: {max_profit}")
+    # print(f"Greatest Decrease in Profits: {min_profit}")
+    # print(type(profit_list)
+
+    for i, amount in enumerate(profit_list):
+        if amount == max_profit:
+            # print(profit_list[i], periods_list[i])
+            total_profit_string = periods_list[i] + " " + str(profit_list[i])
+            break
+    print(f"Greatest Increase in Profits: {total_profit_string}")
+
+    for i, amount in enumerate(profit_list):
+        if amount == min_profit:
+            # print(profit_list[i], periods_list[i])
+            total_profit_string = periods_list[i] + " " + str(profit_list[i])
+            break
+    print(f"Greatest Decrease in Profits: {total_profit_string}")
+    print(f"--------------------------------------------------")
+
+# Define the function and have it accept the 'budget_data' as its sole parameter
+#  def analysis(budget_data):
+#      total_months = int(budget_data[1]))
+#      total_profit = sum(int(budget_data[1]))
+#      max_profit = max(float(budget_data[1]))
+#      min_profit = min(float(budget_data[1]))
 
 # The net total amount of "Profit/Losses" over the entire period
 
@@ -12,57 +91,36 @@
 
 # The greatest decrease in losses (date and amount) over the entire period
 
-# As an example, your analysis should look similar to the one below:
 
-#   Financial Analysis
-#   ----------------------------
-#   Total Months: 86
-#   Total: $38382578
-#   Average  Change: $-2315.12
-#   Greatest Increase in Profits: Feb-2012 ($1926159)
-#   Greatest Decrease in Profits: Sep-2013 ($-2196167)
+# Print analytical stats to the screen
+# print(f"Total Months: {total_months}")
+# print(f"Total: ${profit}")
+# #print(f"Avg Change: {avg}")
+# print(f"Greatest Profit Increase: {max_profit}")
+# print(f"Greatest Profit Decrease: {min_profit}")
 
-# In addition, your final script should both print the analysis to the terminal and export a text file with the results.
 
-# First we'll import the os module
-# This will allow us to create file paths across operating systems
-import os
+# Specify the file path to write to
+# output_path = os.path.join("..", "python-challenge", "PyBank", "Analysis", "financial_analysis.csv")
 
-# Module for reading CSV files so we don't have to parse manually
-import csv
-
-csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
-print(csvpath)
-
-with open(csvpath, 'r') as csvfile:
-
-    # CSV reader specifies delimiter and variable that holds contents
-    csvreader = csv.reader(csvfile, delimiter=',')
-    print("{csv_header}")
-
-    # Read each row of data after the header
-     for row in csvreader:
-         def 
-        #  if float(row[7]) >= 5:
-        #  print(row)
-
-        fiber = float(row[7])
-        if fiber >= 5:
-            print(row)
-
-# Specify the file to write to
-# output_path = os.path.join("..", "Analysis", "financial_analysis.csv")
-
-# Open the file using "write" mode. Specify the variable to hold the contents
+# Write data to a .csv file
 # with open(output_path, 'w', newline='') as csvfile:
+#     csvwriter = csv.writer(csvfile)
 
-#     # Initialize csv.writer
-#     csvwriter = csv.writer(csvfile, delimiter=',')
+# To save specific data input as a row in the csv
+    # writer.writerow(["row1", "row2"])
 
-#     # Write the first row (column headers)
-#     csvwriter.writerow(['First Name', 'Last Name', 'SSN'])
 
-#     # Write the second row
-#     csvwriter.writerow(['Caleb', 'Frost', '505-80-2901'])
+# Write the first row (column headers)
+# csvwriter.writerow(['First Name', 'Last Name', 'SSN'])
 
-         
+# Write the second row
+# csvwriter.writerow(['Caleb', 'Frost', '505-80-2901'])
+
+# Write updated data to csv file
+# csvpath = os.path.join("output", filename)
+# with open(csvpath, "w") as csvfile:
+#     fieldnames = ["last_name", "first_name", "ssn", "email"]
+#     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+#     writer.writeheader()
+#     writer.writerows(new_employee_data)
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,20 +1,14 @@
 # In this challenge, you are tasked with helping a small, rural town modernize its vote counting process.
 
 # You will be give a set of poll data called [election_data.csv](PyPoll/Resources/election_data.csv). The dataset is composed of three columns: `Voter ID`, `County`, and `Candidate`. Your task is to create a Python script that analyzes the votes and calculates each of the following:
-
 # The total number of votes cast
-
 # A complete list of candidates who received votes
-
 # The percentage of votes each candidate won
-
 # The total number of votes each candidate won
-
 # The winner of the election based on popular vote.
 
 # As an example, your analysis should look similar to the one below:
 
-#   ```text
 #   Election Results
 #   -------------------------
 #   Total Votes: 3521001
@@ -26,47 +20,78 @@
 #   -------------------------
 #   Winner: Khan
 #   -------------------------
-#   ````
 
 # In addition, your final script should both print the analysis to the terminal and export a text file with the results.
-# First we'll import the os module
-# This will allow us to create file paths across operating systems
+
+# Import the os and csv modules
 import os
-
-# Module for reading CSV files so we don't have to parse manually
 import csv
+import statistics
 
-csvpath = os.path.join('..', 'Resources', 'election_data.csv')
-print(csvpath)
+# Path to my csv file
+csvpath = os.path.join('..','PyPoll', 'Resources', 'election_data.csv')
+# print(csvpath)
 
 with open(csvpath) as csvfile:
 
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
 
-    print(csvreader)
-
     # Read the header row first (skip this step if there is no header)
-    csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
+    header = next(csvreader)
+    print(header)
+    print(list)
+
+# Create the lists to search through
+    voter_list = []
+    candidate_list = ["Correy", "Khan", "Li", "O'Tooley"]
+    total_votes = []
+    # total_votes = 0
+    vote_percent = 0
+
+    # function to get unique values 
+    def unique(candidate_list): 
+      
+    # # Traverse through all list elements 
+    #     for name in candidate_list: 
+    #         # check if exists in unique_list or not 
+    #         if name not in candidate_list: 
+    #             candidate_list.append(name) 
+    # # print list 
+    #     for Name in candidate_list: 
+    #         print(name) 
 
     # Read each row of data after the header
-    for row in csvreader:
-        print(row)
+        for i, row in enumerate(csvreader):
+            voter_list.append(row[0])
+            # candidate_list.append(row[2])
+            if name not in candidate_list: 
+                candidate_list.append(name) 
+        # print list 
+        for Name in candidate_list: 
+    #         print(name) 
+            total_votes = len(voter_list)
+            print(total_votes)
+            print(candidate_list[2])
 
 # Store the file path associated with the file (note the backslash may be OS specific)
-# file = '../Resources/input.txt'
+# file = 'PyPoll/Analysis/PyPollAnalysis.csv'
 
-# # Open the file in "read" mode ('r') and store the contents in the variable "text"
-# # with / open <file name> 'r' read file as text
+# Open the file in "read" mode ('r') and store the contents in the variable "text"
 # with open(file, 'w') as csv:
 
-#     # This stores a reference to a file stream
+# This stores a reference to a file stream
 #     print(text)
 
-#     # Store all of the text inside a variable called "lines"
+# Store all of the text inside a variable called "lines"
 #     lines = text.read()
 
-#     # Print the contents of the text file to "lines"
+# Print the contents of the text file to "lines"
 #     print(lines)
+
+    # Creating a dictionary
+    # election_results = {'Total Votes': 3, 'Candidate Name': 5, 'Winner': 1}
+    # Printing the dictionary
+    # print(election_results)
+
 
